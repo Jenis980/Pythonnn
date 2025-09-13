77fed46e155f296c0d49e032f3453823a1b2796a
jenis980 <jenis.dangol@gmail.com>
2025-09-13 23:08:04 +0545

    organizing files usig folders

--- windows-device/function/pets.py
+++ windows-device/function/pets.py
@@ -1,10 +1,8 @@
-def describe_pet(animal_type='dog', pet_name='willie'):
-  '''Display information about a pet.'''
-  print("\nI have a " + animal_type + ".")
-  print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-  # positional arguments which means each argument should be on same order as def function parameters
-describe_pet('hamster', 'harry')
-describe_pet('dog', 'willie')
-  # keyword arguments which means each argument should be as name-value pair. it does not concern with order.
-describe_pet(pet_name='harry', animal_type='hamster')
-describe_pet('willie')
+pets = ['dog', 'cat', 'dog', 'goldfish', 'rabbit', 'cat', 'camel', 'cat']
+print(pets)
+
+while 'cat' in pets:
+  pets.remove('cat')
+print(pets)
+
+# above program removes the 'cat' from the list using while loop and remove() method of list.
