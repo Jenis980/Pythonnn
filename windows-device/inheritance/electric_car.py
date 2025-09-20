2e63f249dd2bb7d4ce2b02289e8a5309bbf56bc5
jenis980 <jenis.dangol@gmail.com>
2025-09-20 11:25:51 +0545

    refactor(electric-car): extract Battery class and improve ElectricCar behavior
    - Added a dedicated Battery class to model battery attributes and methods
    - Updated ElectricCar to use Battery instance instead of direct battery size
    - Removed redundant describe_battery from ElectricCar and added electric-specific fill_gas_tank
    - Enhanced code organization and separation of concerns for better maintainability

--- windows-device/inheritance/electric_car.py
+++ windows-device/inheritance/electric_car.py
@@ -16,7 +16,7 @@ class Car():
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
     def fill_gas_tank(self):
-        print("")        
+        print(" ")        
         
     def update_odometer(self, mileage):
         if mileage >= self.odometer_reading:
@@ -27,20 +27,41 @@ class Car():
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+class Battery():
+    """A simple attempt to model a battery for an electric car."""        
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+            """Print a statement describing the battery size."""
+            print("This car has a " + str(self.battery_size) + "-kWh battery.")
+    def get_range(self):
+        """Print a statement about the range this battery provides."""            
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."      
+        print(message)      
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year): # it is done for initailization of Sub class
         super().__init__(make, model, year) # it gives all the Attributes of its parent class
-        self.batterysize = 70 # new attributes to differentiate the child class form the parent class
+        self.battery = Battery()
 
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print("This car has a " + str(self.batterysize) + "-kWh battery.")
-
+    def fill_gas_tank(self):
+        """Electic cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
         
         
         
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())     
-my_tesla.describe_battery() 
+my_tesla.battery.describe_battery() 
+my_tesla.fill_gas_tank()
+my_tesla.battery.get_range()
