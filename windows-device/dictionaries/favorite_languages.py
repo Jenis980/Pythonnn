1b579bb0a3c20d78387589e67b2e25fa151977c3
jenis980 <jenis.dangol@gmail.com>
2025-09-21 08:22:40 +0545

    refactor(dictionaries): switch favorite_languages to OrderedDict for ordered iteration
    - Replaced dict literal with OrderedDict to preserve insertion order
    - Minor formatting fixes for consistency

--- windows-device/dictionaries/favorite_languages.py
+++ windows-device/dictionaries/favorite_languages.py
@@ -1,16 +1,16 @@
-
+from collections import OrderedDict
 
   # using the list and dictionary style
-favorite_languages = {
-  'jen': 'python',
-  'sarah': 'c',
-  'edward': 'ruby',
-  'phil': 'python',
-}
+favorite_languages = OrderedDict()
+
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
 friends = ['phil', 'sarah']
 
 for name in favorite_languages.keys():
   print(name.title())
 
   if name in friends:
-    print(" Hi " + name.title() + ", I see your favorite language is " + favorite_languages[name].title() + "!")
+    print("Hi " + name.title() + ", I see your favorite language is " + favorite_languages[name].title() + "!")
